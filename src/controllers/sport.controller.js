8591ac2212484e6081ab82a6aa38f4d31f82e49f
Salipa-Gurung <salipagurung@gmail.com>
2021-11-20 19:48:04 +0545

    add controller for sport crud

---
+++ src/controllers/sport.controller.js
@@ -0,0 +1,40 @@
+const httpStatus = require('http-status');
+const ApiError = require('../utils/ApiError');
+const catchAsync = require('../utils/catchAsync');
+const { sportService } = require('../services');
+
+const createSport = catchAsync(async (req, res) => {
+  const sport = await sportService.createSport(req.body);
+  res.status(httpStatus.CREATED).send(sport);
+});
+
+const getSports = catchAsync(async (req, res) => {
+  const result = await sportService.getSports();
+  res.send(result);
+});
+
+const getSport = catchAsync(async (req, res) => {
+  const sport = await sportService.getSportById(req.params.sportId);
+  if (!sport) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'sport not found');
+  }
+  res.send(sport);
+});
+
+const updateSport = catchAsync(async (req, res) => {
+  const sport = await sportService.updateSportById(req.params.sportId, req.body);
+  res.send(sport);
+});
+
+const deleteSport = catchAsync(async (req, res) => {
+  await sportService.deleteSportById(req.params.sportId);
+  res.status(httpStatus.NO_CONTENT).send();
+});
+
+module.exports = {
+  createSport,
+  getSports,
+  getSport,
+  updateSport,
+  deleteSport,
+};
