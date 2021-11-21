79374b96bef02edd9b3d6d8edf2680b327f74618
Salipa-Gurung <salipagurung@gmail.com>
2021-11-21 19:52:30 +0545

    add validation for notice CRUD

---
+++ src/validations/notice.validation.js
@@ -0,0 +1,32 @@
+const Joi = require('joi');
+const { objectId } = require('./custom.validation');
+
+const createNotice = {
+  body: Joi.object().keys({
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+  }),
+};
+
+const updateNotice = {
+  params: Joi.object().keys({
+    noticeId: Joi.required().custom(objectId),
+  }),
+  body: Joi.object()
+    .keys({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+    })
+};
+
+const deleteNotice = {
+  params: Joi.object().keys({
+    noticeId: Joi.required().custom(objectId),
+  }),
+}
+
+module.exports = {
+  createNotice,
+  updateNotice,
+  deleteNotice,
+};
