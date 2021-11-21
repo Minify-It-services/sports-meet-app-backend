49cd06919d7ed1486bc8f237cb4dfffab9d13b8d
Salipa-Gurung <salipagurung@gmail.com>
2021-11-21 19:50:24 +0545

    add notice model

--- src/models/index.js
+++ src/models/index.js
@@ -1,3 +1,4 @@
 module.exports.Token = require('./token.model');
 module.exports.User = require('./user.model');
 module.exports.Sport = require('./sport.model');
+module.exports.Notice = require('./notice.model')
---
+++ src/models/notice.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const { toJSON, paginate } = require('./plugins');
+
+const noticeSchema = mongoose.Schema(
+  {
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// add plugin that converts mongoose to json
+noticeSchema.plugin(toJSON);
+noticeSchema.plugin(paginate);
+
+const Notice = mongoose.model('Notice', noticeSchema);
+
+module.exports = Notice;
