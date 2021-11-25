5556b8f6b21d972e73c7bf07dcaf11db24df5415
Alson33 <garbuja12alson@gmail.com>
2021-11-25 14:23:25 +0545

    use jsend for sending request

--- src/controllers/sport.controller.js
+++ src/controllers/sport.controller.js
@@ -2,10 +2,11 @@ const httpStatus = require('http-status');
 const ApiError = require('../utils/ApiError');
 const catchAsync = require('../utils/catchAsync');
 const { sportService } = require('../services');
+const { jsend } = require('../utils/jsend')
 
 const createSport = catchAsync(async (req, res) => {
   const sport = await sportService.createSport(req.body);
-  res.status(httpStatus.CREATED).send(sport);
+  res.status(httpStatus.CREATED).send(jsend(sport));
 });
 
 const getSports = catchAsync(async (req, res) => {
@@ -18,12 +19,12 @@ const getSport = catchAsync(async (req, res) => {
   if (!sport) {
     throw new ApiError(httpStatus.NOT_FOUND, 'sport not found');
   }
-  res.send(sport);
+  res.send(jsend(sport));
 });
 
 const updateSport = catchAsync(async (req, res) => {
   const sport = await sportService.updateSportById(req.params.sportId, req.body);
-  res.send(sport);
+  res.send(jsend(sport));
 });
 
 const deleteSport = catchAsync(async (req, res) => {
