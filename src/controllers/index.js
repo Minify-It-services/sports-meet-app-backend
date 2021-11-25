0cce6365fa43b07744742d94cd3b080945bccc15
Alson33 <garbuja12alson@gmail.com>
2021-11-25 14:23:03 +0545

    add controllers for team CRUD

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,3 +2,4 @@ module.exports.authController = require('./auth.controller');
 module.exports.userController = require('./user.controller');
 module.exports.sportController = require('./sport.controller');
 module.exports.noticeController = require('./notice.controller')
+module.exports.teamController = require('./team.controller')
