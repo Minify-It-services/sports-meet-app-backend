7d2b5e1116b87d2dd6f939ab3c39f83141db4269
Alson33 <garbuja12alson@gmail.com>
2021-11-25 14:22:03 +0545

    add team model

--- src/models/index.js
+++ src/models/index.js
@@ -2,3 +2,4 @@ module.exports.Token = require('./token.model');
 module.exports.User = require('./user.model');
 module.exports.Sport = require('./sport.model');
 module.exports.Notice = require('./notice.model')
+module.exports.Team = require('./team.model')
