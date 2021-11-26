87083c44ba6e34cdafcddd414c506ef950a91337
Salipa-Gurung <salipagurung@gmail.com>
2021-11-26 20:59:18 +0545

    test

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,4 +2,5 @@ module.exports.authController = require('./auth.controller');
 module.exports.userController = require('./user.controller');
 module.exports.sportController = require('./sport.controller');
 module.exports.noticeController = require('./notice.controller')
-module.exports.teamController = require('./team.controller')
+module.exports.teamController = require('./team.controller');
+module.exports.matchController = require('./match.controller');
