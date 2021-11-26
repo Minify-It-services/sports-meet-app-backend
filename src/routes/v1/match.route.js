a97729e120b2143dc9f152cb1196750e3ab80195
Salipa-Gurung <salipagurung@gmail.com>
2021-11-26 20:54:19 +0545

    add route for match CRUD

--- src/routes/v1/match.route.js
+++ src/routes/v1/match.route.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const auth = require('../../middlewares/auth');
 const validate = require('../../middlewares/validate');
-// const matchValidation = require('../../validations/match.validation');
-// const matchController = require('../../controllers/match.controller');
+const matchValidation = require('../../validations/match.validation');
+const matchController = require('../../controllers/match.controller');
 
 const router = express.Router();
 
-// router
-//   .route('/')
-//   .get(auth('getMatches'), validate(matchValidation.getMatches), matchController.getMatches);
-
-// router
-//   .route('/:matchId')
-//   .get(auth(['getMatches', 'getMatch']), validate(matchValidation.getMatch), userController.getMatch)
-//   .patch(auth('manageMatch'), validate(matchValidation.updateMatch), matchController.updateMatch)
-//   .delete(auth('manageMatches'), validate(matchValidation.deleteMatch), matchController.deleteMatch);
+router
+  .route('/')
+  .post(validate(matchValidation.createMatch), matchController.createMatch)
+  .get( matchController.getMatches);
+router
+  .route('/:matchId')
+  .get(validate(matchValidation.getMatch), matchController.getMatch)
+  .patch(auth('manageMatch'), validate(matchValidation.updateMatch), matchController.updateMatch)
+  .delete(auth('manageMatches'), validate(matchValidation.deleteMatch), matchController.deleteMatch);
 
 module.exports = router;
 
